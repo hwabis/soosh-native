854f9f76c45b2967915431da93faedd8e80839d5
hwabis <kennysun23@gmail.com>
2025-04-18 23:28:41 -0400

    uh

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,18 +1,2 @@
-include(FetchContent)
-Set(FETCHCONTENT_QUIET FALSE)
-
-FetchContent_Declare(
-    CLI11
-    GIT_REPOSITORY https://github.com/CLIUtils/CLI11.git
-    GIT_TAG v2.5.0
-)
-FetchContent_MakeAvailable(CLI11)
-
-FetchContent_Declare(
-  Boost
-  URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.xz
-  URL_MD5 893b5203b862eb9bbd08553e24ff146a
-  DOWNLOAD_EXTRACT_TIMESTAMP ON
-)
-set(BOOST_INCLUDE_LIBRARIES asio)
-FetchContent_MakeAvailable(Boost)
+find_package(CLI11 REQUIRED)
+find_package(Boost REQUIRED COMPONENTS asio)
