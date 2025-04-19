af5b646aabc89a52b8effc8bce890e94e759eab5
hwabis <kennysun23@gmail.com>
2025-04-18 22:07:21 -0400

    waste entire day on linking boost

--- src/server/lib/server.cc
+++ src/server/lib/server.cc
@@ -1,11 +1,15 @@
 #include "server.h"
+#include <boost/asio.hpp>
 #include <iostream>
+
+namespace ip = boost::asio::ip;
 
 namespace soosh
 {
 
 void Server::Run(int port)
 {
+    ip::tcp::endpoint endpoint(ip::address_v4::any(), static_cast<unsigned short>(port));
 }
 
 void Server::print(const std::string &message) const
