771a577bbd32960885ec4b6967cdfa6197599032
hwabis <kennysun23@gmail.com>
2025-04-18 22:43:06 -0400

    save some time on initial build

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,15 +1,18 @@
 include(FetchContent)
+Set(FETCHCONTENT_QUIET FALSE)
 
 FetchContent_Declare(
-    cli11
+    CLI11
     GIT_REPOSITORY https://github.com/CLIUtils/CLI11.git
     GIT_TAG v2.5.0
 )
-FetchContent_MakeAvailable(cli11)
+FetchContent_MakeAvailable(CLI11)
 
 FetchContent_Declare(
-    boost
-    GIT_REPOSITORY https://github.com/boostorg/boost.git
-    GIT_TAG boost-1.88.0
+  Boost
+  URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.xz
+  URL_MD5 893b5203b862eb9bbd08553e24ff146a
+  DOWNLOAD_EXTRACT_TIMESTAMP ON
 )
-FetchContent_MakeAvailable(boost)
+set(BOOST_INCLUDE_LIBRARIES asio)
+FetchContent_MakeAvailable(Boost)
